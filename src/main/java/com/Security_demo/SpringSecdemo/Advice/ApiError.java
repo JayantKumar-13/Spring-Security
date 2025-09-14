3a9e96ffb95caa6077aef23263608f5994c6ebdd
Jayant Kumar <jayantkumar13ssm@gmail.com>
2025-09-14 16:01:48 +0530

    Updated Read me file

--- src/main/java/com/Security_demo/SpringSecdemo/Advice/ApiError.java
+++ src/main/java/com/Security_demo/SpringSecdemo/Advice/ApiError.java
@@ -1,18 +1,13 @@
 package com.Security_demo.SpringSecdemo.Advice;
-
 import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
-
 public class ApiError {
     private LocalDateTime timeStamp;
     private String error;
     private HttpStatus statusCode;
-
     public ApiError() {
         this.timeStamp = LocalDateTime.now();
     }
-
     public ApiError(String error, HttpStatus statusCode) {
         this();
         this.error = error;

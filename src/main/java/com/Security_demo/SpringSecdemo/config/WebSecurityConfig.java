acbb36ec3aaba57583f38b7db39e81ee5d99fae9
Jayant Kumar <jayantkumar13ssm@gmail.com>
2025-09-15 18:15:36 +0530

    Added Permissions for user

--- src/main/java/com/Security_demo/SpringSecdemo/config/WebSecurityConfig.java
+++ src/main/java/com/Security_demo/SpringSecdemo/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.Security_demo.SpringSecdemo.Filters.JwtAuthFilter;
 import com.Security_demo.SpringSecdemo.entities.enums.Role;
 import com.Security_demo.SpringSecdemo.handlers.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,40 +16,46 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.Security_demo.SpringSecdemo.entities.enums.Permission.*;
+
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig {
-    private final JwtAuthFilter jwtAuthFilter;
 
+    private final JwtAuthFilter jwtAuthFilter;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     private static final String[] PUBLIC_URLS = {
-          "/auth/signup", "/auth/login","/home.html"
+            "/auth/signup", "/auth/login", "/home.html"
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // disable CSRF once
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PUBLIC_URLS).permitAll()
-                        .requestMatchers(HttpMethod.POST , "/posts/**").hasRole(Role.ADMIN.name()) // only admin can create, update or delete the posts
-                        .requestMatchers(HttpMethod.GET , "/posts/**").permitAll() // anyone can see the posts
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class) // jwtAuthFilter passes before UsernamePasswordAuthenticationFilter
-                .oauth2Login(oauth2Config -> oauth2Config.failureUrl("/login?error =true")
-                        .successHandler(oAuth2SuccessHandler));
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(PUBLIC_URLS).permitAll()
+                .requestMatchers(HttpMethod.POST, "/posts/**")
+                    .hasRole(Role.ADMIN.name())
+                .requestMatchers(HttpMethod.GET, "/posts/**")
+                    .hasAnyAuthority(POST_CREATE.name(), POST_VIEW.name())
+                .requestMatchers(HttpMethod.PUT, "/posts/**")
+                    .hasAuthority(POST_UPDATE.name())
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .oauth2Login(oauth2 -> oauth2
+                .failureUrl("/login?error=true")
+                .successHandler(oAuth2SuccessHandler)
+            );
 
         return http.build();
     }
 
-
-
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 }

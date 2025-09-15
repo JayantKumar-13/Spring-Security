98cbbecb7b2c8ad6e3799cdad6fdbc0d88d4fec1
Jayant Kumar <jayantkumar13ssm@gmail.com>
2025-09-15 18:31:46 +0530

    Added permissions for UserDto

--- src/main/java/com/Security_demo/SpringSecdemo/dto/SignupDto.java
+++ src/main/java/com/Security_demo/SpringSecdemo/dto/SignupDto.java
@@ -1,5 +1,6 @@
 package com.Security_demo.SpringSecdemo.dto;
 
+import com.Security_demo.SpringSecdemo.entities.enums.Permission;
 import com.Security_demo.SpringSecdemo.entities.enums.Role;
 import lombok.Data;
 
@@ -11,4 +12,5 @@ public class SignupDto {
     private String password;
     private String name;
     private Set<Role> roles;
+    private Set<Permission> permissions;
 }
